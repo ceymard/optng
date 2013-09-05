facb3854b9037f754e475da188f44da3d8f90011
Christophe Eymard <ceymard@optilian.com>
2013-09-05 12:29:06 +0200

    evol: added ObserverGroup

---
+++ src/utils/ObserverGroup.js
@@ -0,0 +1,34 @@
+angular.module('optng.utils.ObserverGroup', []);
+
+angular.module('optng.utils.ObserverGroup').factory(
+	'$optng.utils.ObserverGroup',
+function () {
+
+	function ObserverGroup(fn) {
+		this.vars = [];
+		this.setCallback(fn);
+	}
+
+	ObserverGroup.prototype.setCallback = function (fn) {
+		var vars = this.vars;
+
+		this.fn = _.debounce(function () {
+			// We use $timeout to trigger re-evaluation of the
+			// rootScope's variables.
+			$timeout(function () {
+				(fn || angular.noop).apply(this, vars);
+			});
+		}, 5);
+	}
+
+	ObserverGroup.prototype.callback = function () {
+		var index = this.vars.length;
+		this.vars.push(null);
+		return _.bind(function (res) {
+			this.vars[index] = res;
+			this.fn();
+		}, this);
+	}
+
+	return ObserverGroup;
+});
---
+++ src/utils/utils.js
@@ -0,0 +1,9 @@
+/*!
+	@module optng.utils
+
+	@description
+		A collection of utils useful for working with angularJS.
+*/
+angular.module('optng.utils', [
+	'optng.utils.ObserverGroup'
+]);
