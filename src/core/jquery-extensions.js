cc57d2afcc163ef4421e6eb361f51a6d11c64742
Christophe Eymard <ceymard@optilian.com>
2013-09-17 17:10:24 +0200

    evol: more angular compliant jquery.

--- src/core/jquery-extensions.js
+++ src/core/jquery-extensions.js
@@ -4,46 +4,48 @@ var module = angular.module('optng.core.jquery-extensions', []);
 
 module.config(function () {
 
-	$.fn.childScopes = function (scopes) {
-		scopes = scopes || [];
+	_.extend(angular.element.prototype, {
+		childScopes: function (scopes) {
+			scopes = scopes || [];
 
-		_.each(this.children(), function (child) {
-			var scope = $(child).scope();
-			var parentscope = $(child).parent().scope();
-			if (scope && scope !== parentscope) {
-				scopes.push(scope);
-			} else {
-				$(child).childScopes(scopes);
-			}
-		});
-
-		return scopes;
-	};
-
-	/**
-
-	*/
-	$.fn.destroy = function () {
-
-		this.each(function (i, node) {
-			node = $(node);
-
-			$.each(node.childScopes(), function (i, scope) {
-				scope.$destroy();
+			angular.forEach(this.children(), function (child) {
+				var scope = $(child).scope();
+				var parentscope = $(child).parent().scope();
+				if (scope && scope !== parentscope) {
+					scopes.push(scope);
+				} else {
+					$(child).childScopes(scopes);
+				}
 			});
 
-			// If the current scope is unique to this element,
-			// destroy it.
-			// TODO: maybe should also destroy the siblings !
-			var current_scope = node.scope();
+			return scopes;
+		},
 
-			if (current_scope !== node.parent().scope())
-				current_scope.$destroy();
+		/**
 
-		});
+		*/
+		destroy: function () {
 
-		return this.remove();
-	}
+			angular.forEach(function (node) {
+				node = $(node);
+
+				angular.forEach(node.childScopes(), function (scope) {
+					scope.$destroy();
+				});
+
+				// If the current scope is unique to this element,
+				// destroy it.
+				// TODO: maybe should also destroy the siblings !
+				var current_scope = node.scope();
+
+				if (current_scope !== node.parent().scope())
+					current_scope.$destroy();
+
+			});
+
+			return this.remove();
+		}
+	});
 
 });
 
