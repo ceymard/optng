7db941047c4766ddfa37ba2ea5fbc970cf6aa0c7
Christophe Eymard <ceymard@optilian.com>
2014-02-03 18:25:50 +0100

    evol: global gestures now also parse attributes

--- src/gestures/directives.js
+++ src/gestures/directives.js
@@ -2,6 +2,25 @@
 
 var module = angular.module('optng.gestures.directives', ['ng', 'optng.gestures.service']);
 
+function parseGestures(attrs) {
+	var gestures = {};
+
+	for (name in attrs) {
+		if (name[0] === '$' || !attrs.hasOwnProperty(name))
+			continue;
+
+		var orig_name = attrs.$attr[name];
+		if (orig_name.indexOf('on-') !== 0)
+			continue;
+		orig_name = orig_name.slice(3);
+
+		var shortcut = orig_name.replace(/-/g, ',').replace(/_/g, '+');
+		gestures[shortcut] = attrs[name];
+	}
+
+	return gestures;
+}
+
 /**
 	@directive gestures
 
@@ -19,18 +38,7 @@ function ($gestures) {
 		link: function (scope, elt, attrs) {
 			var gestures = scope.$eval(attrs.optGestures) || {};
 
-			for (name in attrs) {
-				if (name[0] === '$' || !attrs.hasOwnProperty(name))
-					continue;
-
-				var orig_name = attrs.$attr[name];
-				if (orig_name.indexOf('on-') !== 0)
-					continue;
-				orig_name = orig_name.slice(3);
-
-				var shortcut = orig_name.replace(/-/g, ',').replace(/_/g, '+');
-				gestures[shortcut] = attrs[name];
-			}
+			angular.extend(gestures, parseGestures(attrs));
 
 			$gestures(gestures, scope, elt);
 		}
@@ -62,8 +70,10 @@ function ($gestures) {
 	return {
 		restrict: 'A',
 		link: function (scope, elt, attrs) {
+			var gestures = scope.$eval(attrs.optGlobalGestures) || {};
+			angular.extend(gestures, parseGestures(attrs));
 			// no elt.
-			$gestures(attrs.optGlobalGestures, scope);
+			$gestures(gestures, scope);
 		}
 	};
 
