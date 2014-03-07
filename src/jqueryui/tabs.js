eaef42d8a75beeab350629a5930a052a2491af9d
Christophe Eymard <ceymard@optilian.com>
2014-03-07 18:18:38 +0100

    evol: tabs almost functional

--- src/jqueryui/tabs.js
+++ src/jqueryui/tabs.js
@@ -2,7 +2,8 @@
 
 var module = angular.module('optng.jqueryui.tabs', [
 	'ng',
-	'optng.jqueryui.core'
+	'optng.jqueryui.core',
+	'optng.jqueryui.tabs.templates'
 ]);
 
 /**
@@ -24,28 +25,92 @@ var module = angular.module('optng.jqueryui.tabs', [
 
 		removeTab(key)
 */
+module.directive('jquiTabs',
+['$compile', '$templateCache', '$animate', '$timeout',
+function ($compile, $tc, $animate, $timeout) {
 
-module.directive('jquiTabs', function () {
+	var template = $compile($tc.get('$optng.jqueryui.tab.template'));
 
 	return {
-		templateUrl: '$optng.jqueryui.tab.template',
 		controller: [
-			'$scope',
-			function ($scope) {
+			'$scope', '$element',
+			function ($scope, $element) {
+				var list_clone = null;
+				var ctrl = this;
+				var current_tab_element = null;
+
+				ctrl.current = null;
+
+				$element.addClass('ui-tabs');
+
+				template($scope, function (_list_clone) {
+					// console.log($element, list_clone);
+					// $element.prepend(list_clone);
+					list_clone = _list_clone;
+					$element.append(list_clone);
+				});
+
+				$scope._tabs = this;
+
+				ctrl.tabs = [];
+
+				ctrl.addTab = function addTab(tab) {
+					tab.element = null;
+
+					if (!ctrl.current) {
+
+						$timeout(function () {
+							ctrl.focusTab(tab);
+						});
+
+						ctrl.current = tab;
+					}
+
+					ctrl.tabs.push(tab);
+					// clone.after(tab.element);
+
+				};
+
+				ctrl.focusTab = function focusTab(tab) {
+
+					if (ctrl.current.element) {
+						$animate.leave(ctrl.current.element);
+						ctrl.current.element = null;
+					}
+
+					var current = ctrl.current = tab;
+
+					current.transclude(current.scope, function (tab_clone) {
+						$animate.enter(tab_clone, $element, list_clone);
+						current.element = tab_clone;
+						tab_clone.addClass('ui-tabs-panel');
+					});
+					// fixme
+				};
 
 			}
 		]
 	};
 
-});
+}]);
 
 
 module.directive('jquiTab', function () {
 
 	return {
 		require: '^jquiTabs',
-		link: function (scope, elt, attrs, ctrl) {
+		transclude: 'element',
+		compile: function (elt, attrs, transclude) {
+			elt.addClass('ui-tabs-panel');
 
+			return function (scope, elt, attrs, ctrl) {
+
+				ctrl.addTab({
+					title: attrs.title,
+					transclude: transclude,
+					scope: scope
+				});
+			}
 		}
 	};
 
--- src/jqueryui/tabs.templates.jade.tpl
+++ src/jqueryui/tabs.templates.jade.tpl
@@ -2,19 +2,20 @@
 meta(ng-module='optng.jqueryui.tabs.templates')
 
 script(type='text/ng-template', id='$optng.jqueryui.tab.template')
-	ul.ui-tabs-nav.ui-helper-reset.ui-helper-clearfix.ui-widget-header.ui-corner-all
+	ul.ui-tabs-nav.ui-helper-reset.ui-helper-clearfix.ui-widget-header.ui-corner-all(role='tablist')
 		li(
 			jqui-hoverable
-			ng-class="{'ui-state-active': tab === _tabs.$tab}"
+			ng-class="{'ui-state-active': tab === _tabs.current, 'ui-tabs-active': tab === _tabs.current}"
 			ng-repeat="tab in _tabs.tabs"
 			class="ui-state-default ui-corner-top"
+			role='tab'
 		)
 			a.ui-tabs-anchor(
 				href="javascript://"
 				tabindex='-1'
-				ng-mouseenter="hovering = true"
-				ng-mouseleave="hovering = false"
-				ng-click="_tabs.show(tab)"
+				ng-click="_tabs.focusTab(tab)"
+				class='ui-tabs-anchor'
+				role='presentation'
 			)
 				{{ tab.title }}
 				i.icon-remove(ng-show="tab.closable" ng-click="tab.remove()")
