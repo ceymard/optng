34a92e9dcfd7dc9dd04d71e463aec6b4a6d886f1
Christophe Eymard <ceymard@optilian.com>
2014-03-20 13:20:39 +0100

    work on goodparts

--- src/utils/optng.utils.goodparts.js
+++ src/utils/optng.utils.goodparts.js
@@ -6,35 +6,35 @@ module.factory('optng.utils.goodparts', function () {
 
     var re_super_call = /this.super\s*\(/;
 
-    function Goodparts(fn) {
-        this.obj = fn;
-        this.obj.prototype.constructor = fn;
-    }
-
-    Goodparts.prototype = {
+    var methods = {
         inherits: function inherits(Parent) {
-            this.obj.prototype = new Parent();
-            this.obj.prototype.constructor = this.obj;
-            return this;
-        },
-        method: function method(name, fn) {
-            this.obj.prototype[name] = this.super(fn);
+            this.prototype = new Parent();
+            this.prototype.constructor = this;
             return this;
         },
         methods: function methods(_methods) {
             for (var name in _methods)
                 this.method(name, _methods[name]);
+            return this;
         },
         uses: function uses() {
             // uses is an alias for methods since it just copies
             // methods to the prototype.
             return this.methods.apply(this, arguments);
+        },
+        child: function (sub) {
+            goodparts(sub);
+            sub.inherits(this);
+            return sub;
         }
     };
 
-    return function (obj) {
-        return new Goodparts(obj);
-    };
+    function goodparts(fn) {
+        this.obj = fn;
+        this.obj.prototype.constructor = fn;
+    }
+
+    return goodparts;
 });
 
 })();
